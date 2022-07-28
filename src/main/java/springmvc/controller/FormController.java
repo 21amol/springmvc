13df5b426a34b0293be8027aede5638a306e7c74
Amol <amol9.ag@gmail.com>
2022-07-28 15:24:26 +0530

    ADDED: @ModelAttribute to bind data to Model object

--- src/main/java/springmvc/controller/FormController.java
+++ src/main/java/springmvc/controller/FormController.java
@@ -1,13 +1,27 @@
 package springmvc.controller;
 
+//import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import springmvc.model.User;
+
 @Controller
 public class FormController {
+
+	// Using @ModelAttribute outside the method...
+	//If we use @ModelAttribute on the top of the method then, your Model object becomes common for all methods... 
+	// This method(showCommonData) should be created before the handler method(showForm) so, that it will bind the data before the handler method.
+	@ModelAttribute
+	public void showCommonData(Model m) {
+		m.addAttribute("reg", "Registration Form");
+		m.addAttribute("desc", "Login into the football world");
+		System.out.println("Adding common data");
+	}
 
 	@GetMapping("/form")
 	public String showForm() {
@@ -15,21 +29,39 @@ public class FormController {
 		return "form";
 	}
 
+	// Using @ModelAttribute inside the method
+	@PostMapping("/submitform") // path should be same as action in form.jsp file.
+	public String formHandler(@ModelAttribute User user, Model model) {
+		System.out.println(user);
+		return "success";
+	}
+
 	// In this case your data firstly moves from view to controller and then
 	// secondly, from controller it goes to view part.
-	@PostMapping("/submitform") // path should be same as action in form.jsp file.
-	public String formHandler(@RequestParam String userName, @RequestParam String email, @RequestParam String password,
-			Model model) {
 
-		System.out.println("User Name : " + userName);
-		System.out.println("User Email : " + email);
-		System.out.println("User Password : " + password);
+	/*
+	 * @PostMapping("/submitform") // path should be same as action in form.jsp
+	 * file. public String formHandler(@RequestParam String userName, @RequestParam
+	 * String email, @RequestParam String password, Model model) {
+	 * 
+	 * 
+	 * System.out.println("User Name : " + userName);
+	 * System.out.println("User Email : " + email);
+	 * System.out.println("User Password : " + password);
+	 * 
+	 * 
+	 * User user = new User(); user.setUserName(userName);
+	 * user.setPassword(password); user.setEmail(email); System.out.println(user);
+	 * model.addAttribute("user", user);
+	 * 
+	 * 
+	 * model.addAttribute("name", userName); model.addAttribute("email", email);
+	 * model.addAttribute("password", password);
+	 * 
+	 * 
+	 * return "success";
+	 * 
+	 * }
+	 */
 
-		model.addAttribute("name", userName);
-		model.addAttribute("email", email);
-		model.addAttribute("password", password);
-
-		return "success";
-
-	}
 }
--- src/main/webapp/WEB-INF/views/success.jsp
+++ src/main/webapp/WEB-INF/views/success.jsp
@@ -10,11 +10,14 @@
 <title>Success</title>
 </head>
 <body>
-	<h1>Welcome, ${name }.</h1>
+	<h1 class="text-center">${reg }</h1>
+	<h2>${desc }</h2>
 	<hr>
-	<h1>Your email address is ${email }.</h1>
+	<h1>Welcome, ${user.userName }.</h1>
 	<hr>
-	<h1>Your password is ${password }.</h1>
+	<h1>Your email address is ${user.email }.</h1>
+	<hr>
+	<h1>Your password is ${user.password }.</h1>
 	<h2>Caution!!!. Don't share your password with anyone.</h2>
 </body>
 </html>
